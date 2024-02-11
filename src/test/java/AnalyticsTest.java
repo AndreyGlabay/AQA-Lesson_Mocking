a26b3417b399b77956e595994a5e5c183065b340
Andriy Glabay <rishelevsky@gmail.com>
2024-02-12 01:41:34 +0200

    Test Class AnalyticsTest and Task-14-2.txt: correct some comments with solution explanation.

--- src/test/java/AnalyticsTest.java
+++ src/test/java/AnalyticsTest.java
@@ -1,5 +1,3 @@
-//package java.test; // (2.1.a) In the dir src/test/java create new package "com.example.school.tests";
-
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
@@ -12,16 +10,16 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests" create new test class "AnalyticsTest";
-    int port = 9098; // (2.1.g) Implements var "port" and initiate it;
-    OkHttpClient client = new OkHttpClient.Builder().build(); // (2.1.c) Implements new OkHttpClient;
+public class AnalyticsTest { // (2.1.a) In the package "com.example.school.tests" create new test class "AnalyticsTest";
+    int port = 9098; // (2.1.f) Implements var "port" and initiate it;
+    OkHttpClient client = new OkHttpClient.Builder().build(); // (2.1.b) Implements new OkHttpClient;
     WireMockServer wireMockServer
-            = new WireMockServer(new WireMockConfiguration().port(9098));//(2.1.d)Implements new WireMockServer;
+            = new WireMockServer(new WireMockConfiguration().port(9098));//(2.1.c)Implements new WireMockServer;
 
-    @BeforeClass // (2.1.e) Implements @BeforeClass annotation
+    @BeforeClass // (2.1.d) Implements @BeforeClass annotation
     public void beforeClass(){
-        wireMockServer.start(); // (2.1.f) Implements start of wireMockServer;
-        WireMock.configureFor(port); // (2.1.g) Implements configuration creating using port num;
+        wireMockServer.start(); // (2.1.e) Implements start of wireMockServer;
+        WireMock.configureFor(port); // (2.1.f) Implements configuration creating using port num;
         WireMock.stubFor( // (2.2.a) Configure stubbing behavior: ->
                 WireMock.get( // -> set up behavior for Request HTTP-Method GET ->
                         WireMock.urlEqualTo("/analytics") // -> and set up behavior for Request URL ->
@@ -30,9 +28,9 @@ public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests
         );
     }
 
-    @AfterClass // (2.1.e) Implements @AfterClass annotation
+    @AfterClass // (2.1.d) Implements @AfterClass annotation
     public void afterClass(){
-        wireMockServer.stop(); // (2.1.f) Implements stop of wireMockServer;
+        wireMockServer.stop(); // (2.1.g) Implements stop of wireMockServer;
     }
 
     @Test
