6a86fb536aa9e91b86dc091730ddb9123242fa97
Andriy Glabay <rishelevsky@gmail.com>
2024-02-12 14:56:03 +0200

    (1) Annotation @BeforeClass: modify stubbing - set Header 'authorization' with value 'Basic' for responce; (2) Annotation @Test: add Assertion for check Header matching.

--- src/test/java/AnalyticsTest.java
+++ src/test/java/AnalyticsTest.java
@@ -14,17 +14,31 @@ public class AnalyticsTest { // (2.1.a) In the package "com.example.school.tests
     int port = 9098; // (2.1.f) Implements var "port" and initiate it;
     OkHttpClient client = new OkHttpClient.Builder().build(); // (2.1.b) Implements new OkHttpClient;
     WireMockServer wireMockServer
-            = new WireMockServer(new WireMockConfiguration().port(9098));//(2.1.c)Implements new WireMockServer;
+            = new WireMockServer(new WireMockConfiguration().port(9098)); // (2.1.c) Add new WireMockServer;
+
+    int delay = 3731;
+    int code = 214;
+
+
+
+
 
     @BeforeClass // (2.1.d) Implements @BeforeClass annotation
     public void beforeClass(){
         wireMockServer.start(); // (2.1.e) Implements start of wireMockServer;
-        WireMock.configureFor(port); // (2.1.f) Implements configuration creating using port num;
-        WireMock.stubFor( // (2.2.a) Configure stubbing behavior: ->
-                WireMock.get( // -> set up behavior for Request HTTP-Method GET ->
-                        WireMock.urlEqualTo("/analytics") // -> and set up behavior for Request URL ->
-                ).willReturn(WireMock.aResponse().withStatus(200)) // (2.2.b) If Request matched with listed ->
-                // -> in the (step 2.2.a) - WireMock returns this object with status code = 200;
+        WireMock.configureFor("localhost", port); // (2.1.f) Implements config creating using localhost & port #;
+        WireMock.stubFor( // (2.2.a) Configure stubbing behavior for Request with ->
+                WireMock.get(WireMock.urlEqualTo("/analytics")) // -> with Request HTTP-Method GET;
+                        .willReturn(WireMock.aResponse() // (2.2.b) If req matched with URL - WireMock returns:
+                                .withHeader("authorization", "Basic") // (2.3.a) Set the Header for resp.
+                                .withStatus(code) //  status code = 200;
+                                .withFixedDelay(delay)
+
+
+                        )
+
+
+
         );
     }
 
@@ -34,13 +48,14 @@ public class AnalyticsTest { // (2.1.a) In the package "com.example.school.tests
     }
 
     @Test
-    public void checkMyUrl() throws IOException { // (2.2.c) Implements method "checkMyUrl()" and ->
+    public void checkMyResponse() throws IOException { // (2.2.c) Implements method "checkMyUrl()" and ->
         var myUrl = "http://localhost:" + port + "/analytics"; // -> var = myURL (URL+port+Endpoint) ->
         var request = new Request.Builder() // -> and create new request;
                 .url(myUrl)
                 .build();
         try (var response = client.newCall(request).execute()) { // (2.2.d) Execute this new request;
             Assert.assertEquals(request.url().toString(), myUrl); // (2.2.e) Check actual URL = expected URL.
+            Assert.assertEquals("Basic", response.header("authorization")); // (2.3.b) Check Header matching;
             // Assert.assertEquals(response.code(), 200);
         }
     }
